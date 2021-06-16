e09d932b599e61be2518b8cb2009fb9a48b75c26
Oleksandr Antonkin <Oleksandr_Antonkin@epam.com>
2021-06-16 18:52:18 +0300

    - added logging

--- src/main/java/TestFile.java
+++ src/main/java/TestFile.java
@@ -1,16 +1,41 @@
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
+
+@Slf4j
+@Getter
 public class TestFile {
+
+    /*
+    * Regarding task, not sure if array should contain unique values.
+    * */
+    String log4jConfPath = "log4j.properties";
+
+    public void printIt(List<Integer> listOfValues, Integer number) {
+        log.info(" ======================= ");
+        log.info("Results for number {} in {}", number, listOfValues);
+
+        log.info("Vanilla:\t\t {}", this.vanillaM(listOfValues, number));
+        log.info("Streams:\t\t {}", this.streamM(listOfValues, number));
+    }
+
     public static void main(String[] args) {
-        List<Integer> listOfValues = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8);
-        Integer number = 2;
         TestFile obj = new TestFile();
-        System.out.println(obj.vanillaM(listOfValues, number));
-        System.out.println(obj.streamM(listOfValues, number));
+        PropertyConfigurator.configure(obj.getLog4jConfPath());
+        List<Integer> listOfValues = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8);
+        obj.printIt(listOfValues, 2);
+        obj.printIt(listOfValues, 5);
+        listOfValues = Arrays.asList(2, 2, 2, 3, 4, 4, 5, 6, 7, 8);
+        obj.printIt(listOfValues, 6);
+        obj.printIt(listOfValues, 8);
     }
 
     private ArrayList<List<Integer>> vanillaM(List<Integer> listOfValues, Integer number) {
